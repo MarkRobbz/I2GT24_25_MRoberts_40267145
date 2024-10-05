d119e702010f973f49725ecefe3a598cabfe7c01
Mark Roberts <61202394+MarkRobbz@users.noreply.github.com>
2024-10-05 19:03:19 +0100

    Challenge 2 Complete

---
+++ Create with Code Part 1 Mission 1 - Player Control/Prototype2/Prototype2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private bool canSpawn = true;  
+
+    [SerializeField] private float spawnCooldown = 2.0f;  
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && canSpawn)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            canSpawn = false;  
+            Invoke("EnableSpawning", spawnCooldown);  
+        }
+    }
+
+    
+    private void EnableSpawning()
+    {
+        canSpawn = true;
+    }
+}
