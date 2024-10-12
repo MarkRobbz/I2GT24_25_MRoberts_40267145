e9ea743de7cee6ad1d74e5236af6b64e889f2510
Mark Roberts <61202394+MarkRobbz@users.noreply.github.com>
2024-10-12 15:34:53 +0100

    Player moue movement (Basic player controller Complete)
    Player can use mouse to look now and normlised movement when walking diagonal.

--- Project/Anteloper/Assets/Scripts/PlayerController.cs
+++ Project/Anteloper/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Searcher;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,29 +8,60 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private CharacterController _playerController;
 
     [Header("Movement Speed")] 
-    [SerializeField] private float _walkSpeed = 2.0f;
+    [SerializeField] private float _walkSpeed = 3.0f;
 
+    [Header("Look Sensitivity")] 
+    [SerializeField] private float _mouseSensitivity = 2.0f;
+    [SerializeField] private float _lookUpandDownRange = 80.0f;
+    
     [Header("Custom Inputs")] 
     [SerializeField] private String _horizontalMovementInput = "Horizontal";
     [SerializeField] private String _verticalMovementInput = "Vertical";
+    [SerializeField] private String _lookRotaionXInput = "Mouse X";
+    [SerializeField] private String _lookRotaionYInput = "Mouse Y";
+    
+    private float _verticalRotation;
+    private Camera mainCamera;
+    
     void Start()
     {
         _playerController = gameObject.GetComponent<CharacterController>();
+        mainCamera = Camera.main;
     }
     
     void Update()
     {
         HandleMovement();
+        HandleLookRotation();
     }
 
     void HandleMovement()
     {
-        float verticalSpeed = Input.GetAxis(_verticalMovementInput) * _walkSpeed;
-        float horizontalSpeed = Input.GetAxis(_horizontalMovementInput) * _walkSpeed;
+        float verticalInput = Input.GetAxis(_verticalMovementInput);
+        float horizontalInput = Input.GetAxis(_horizontalMovementInput);
 
-        Vector3 speed = new Vector3(horizontalSpeed, 0, verticalSpeed);
-        speed = transform.rotation * speed;
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+        
+        // Normalise movement vector if magnitude is greater than 1
+        if (movement.magnitude > 1f)
+        {
+            movement.Normalize();
+        }
+        
+        movement = transform.rotation * movement;
+        
+        Vector3 speed = movement * _walkSpeed;
         
         _playerController.SimpleMove(speed);
+    }
+
+    void HandleLookRotation()
+    {
+        float xRotation = Input.GetAxis(_lookRotaionXInput) * _mouseSensitivity ;
+        transform.Rotate(0,xRotation,0);
+        
+        _verticalRotation -= Input.GetAxis(_lookRotaionYInput) * _mouseSensitivity ;
+        _verticalRotation = Mathf.Clamp(_verticalRotation, -_lookUpandDownRange, _lookUpandDownRange); //Clamp looking up and down
+        mainCamera.transform.localRotation = Quaternion.Euler(_verticalRotation,0,0);
     }
 }
