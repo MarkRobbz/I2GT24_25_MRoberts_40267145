0a47153dc60903d9c5a3a30d4829b4626bce94d0
Mark Roberts <61202394+MarkRobbz@users.noreply.github.com>
2024-10-15 21:06:47 +0100

    Merge lost my previous code, amending...

--- Project/Anteloper/Assets/Scripts/PlayerController.cs
+++ Project/Anteloper/Assets/Scripts/PlayerController.cs
@@ -9,16 +9,12 @@ public class PlayerController : MonoBehaviour
 
     [Header("Movement Speed")] 
     [SerializeField] private float _walkSpeed = 3.0f;
-
-
-
     [SerializeField] private float _sprintMultiplier = 2.0f;
 
     [Header("Jumping")] 
-    [SerializeField] private float jumpForce = 5.0f;
-    [SerializeField] private float gravity = 9.81f; //Earths gravity
+    [SerializeField] private float _gravity = 9.18f; // Earth's Gravity
+    [SerializeField] private float _jumpingForce = 6.0f;
     
-
     [Header("Look Sensitivity")] 
     [SerializeField] private float _mouseSensitivity = 2.0f;
     [SerializeField] private float _lookUpAndDownRange = 80.0f;
@@ -26,59 +22,80 @@ public class PlayerController : MonoBehaviour
     [Header("Custom Inputs")] 
     [SerializeField] private String _horizontalMovementInput = "Horizontal";
     [SerializeField] private String _verticalMovementInput = "Vertical";
-    [SerializeField] private String _lookRotaionXInput = "Mouse X";
-    [SerializeField] private String _lookRotaionYInput = "Mouse Y";
-
+    [SerializeField] private String _lookRotationXInput = "Mouse X";
+    [SerializeField] private String _lookRotationYInput = "Mouse Y";
     [SerializeField] private KeyCode _sprintKeyInput = KeyCode.LeftShift;
-    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
-    
-
+    [SerializeField] private KeyCode _jumpKey = KeyCode.Space;
     
     private float _verticalRotation;
-    private Camera mainCamera;
-    
+    private Vector3 _currentMovement = Vector3.zero;
+    private Camera _mainCamera;
     
     void Start()
     {
         _playerController = gameObject.GetComponent<CharacterController>();
-        mainCamera = Camera.main;
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        _mainCamera = Camera.main;
     }
     
     void Update()
     {
         HandleMovement();
         HandleLookRotation();
+        HandleJumpAndGravity(); 
     }
 
     void HandleMovement()
     {
+        float speedMultiplier = Input.GetKey(_sprintKeyInput) ? _sprintMultiplier : 1f;
         float verticalInput = Input.GetAxis(_verticalMovementInput);
         float horizontalInput = Input.GetAxis(_horizontalMovementInput);
 
         Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
         
-        // Normalise movement vector if magnitude is greater than 1
+        // Normalize movement vector if magnitude is greater than 1 (diagonal movement)
         if (movement.magnitude > 1f)
         {
             movement.Normalize();
         }
         
+        movement *= _walkSpeed * speedMultiplier;
+        
         movement = transform.rotation * movement;
         
-        Vector3 speed = movement * _walkSpeed;
+        // Update horizontal movement
+        _currentMovement.x = movement.x;
+        _currentMovement.z = movement.z;
         
-        _playerController.SimpleMove(speed);
+        
+        _playerController.Move(_currentMovement * Time.deltaTime);
     }
 
     void HandleLookRotation()
     {
-        float xRotation = Input.GetAxis(_lookRotaionXInput) * _mouseSensitivity ;
-        transform.Rotate(0,xRotation,0);
+        float xRotation = Input.GetAxis(_lookRotationXInput) * _mouseSensitivity;
+        transform.Rotate(0, xRotation, 0); 
         
-        _verticalRotation -= Input.GetAxis(_lookRotaionYInput) * _mouseSensitivity ;
-        _verticalRotation = Mathf.Clamp(_verticalRotation, -_lookUpAndDownRange, _lookUpAndDownRange); //Clamp looking up and down
-        mainCamera.transform.localRotation = Quaternion.Euler(_verticalRotation,0,0);
+        _verticalRotation -= Input.GetAxis(_lookRotationYInput) * _mouseSensitivity;
+        _verticalRotation = Mathf.Clamp(_verticalRotation, -_lookUpAndDownRange, _lookUpAndDownRange); // Clamp looking up and down
+        
+        _mainCamera.transform.localRotation = Quaternion.Euler(_verticalRotation, 0, 0); 
+    }
+
+    void HandleJumpAndGravity()
+    {
+   
+        if (_playerController.isGrounded)
+        {
+            _currentMovement.y = -0.5f; // Keep grounded
+            if (Input.GetKeyDown(_jumpKey))
+            {
+                _currentMovement.y = _jumpingForce; 
+            }
+        }
+        else
+        {
+            // Apply gravity while in the airs
+            _currentMovement.y -= _gravity * Time.deltaTime;
+        }
     }
 }
