48a7e564fc9bc2df1c5ed12ce832604a3afa6b4d
MarkRobbz <markrobb259@gmail.com>
2024-10-17 20:34:28 +0100

    Interaction using Sphere casting
    Changed to sphere cast for smaller objects.
    *Might need to configure constant ray casting incase of performance issues.

--- Project/Anteloper/Assets/Scripts/Interaction.cs
+++ Project/Anteloper/Assets/Scripts/Interaction.cs
@@ -1,43 +1,61 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Interaction : MonoBehaviour
 {
     [SerializeField] private Camera _playerCamera;
-    [SerializeField] private float _rayDistance = 3.0f;
-    [SerializeField] private LayerMask _interactableLayer =  1 << 8;
+    [SerializeField] private float _rayCastDistance = 3.0f;
+    [SerializeField] private LayerMask _interactableLayer = 1 << 8;  
     [SerializeField] private KeyCode _interactKey = KeyCode.E;
+    [SerializeField] private TextMeshProUGUI interactionUI; 
+
+    private IUsable _currentUsable;  
 
     private void Start()
     {
+        // Get the player camera and the interaction UI element
         _playerCamera = gameObject.GetComponentInChildren<Camera>();
+        interactionUI = GameObject.FindGameObjectWithTag("InteractPromptUI").GetComponentInChildren<TextMeshProUGUI>();
+
+        // Initially hide the interaction prompt
+        interactionUI.gameObject.SetActive(false);
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(_interactKey))
+        DetectInteractableObject();
+        
+        if (Input.GetKeyDown(_interactKey) && _currentUsable != null)
         {
-            TryInteract(); 
+            _currentUsable.Use(); 
         }
     }
 
-
-    void TryInteract()
+    void DetectInteractableObject()
     {
         RaycastHit hit;
         
-        Debug.DrawRay(_playerCamera.transform.position, _playerCamera.transform.forward * _rayDistance, Color.red, 1.0f);
-        
-        if (Physics.Raycast(_playerCamera.transform.position, _playerCamera.transform.forward, out hit, _rayDistance, _interactableLayer))
+   
+        Debug.DrawRay(_playerCamera.transform.position, _playerCamera.transform.forward * _rayCastDistance, Color.red, 0.5f);
+
+        if (Physics.Raycast(_playerCamera.transform.position, _playerCamera.transform.forward, out hit, _rayCastDistance, _interactableLayer))
         {
             IUsable usable = hit.collider.GetComponent<IUsable>();
 
             if (usable != null)
             {
-                usable.Use(); // Call the Use() on interactable object
+                _currentUsable = usable;
+                interactionUI.text = "Press E to Pickup";  
+                interactionUI.gameObject.SetActive(true);  
             }
         }
-}
+        else
+        {
+            _currentUsable = null;
+            interactionUI.gameObject.SetActive(false);  
+        }
+    }
 }
