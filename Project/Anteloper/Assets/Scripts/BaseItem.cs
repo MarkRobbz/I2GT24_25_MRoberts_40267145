3104150905513099c044f1492f637632b48b1a27
MarkRobbz <markrobb259@gmail.com>
2024-10-21 14:44:07 +0100

    Inventory Scriptable objects created

--- Project/Anteloper/Assets/Scripts/BaseItem.cs
+++ Project/Anteloper/Assets/Scripts/BaseItem.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CraftableItem : MonoBehaviour, IUsable
+
+[CreateAssetMenu(fileName = "NewItem", menuName = "Inventory/BaseItem")]
+public class BaseItem : ScriptableObject, IUsable
 {
     public string itemName; 
     public Sprite itemIcon; 
-    public BaseItem[] requiredItems; 
-    public int[] requiredItemCounts; 
+    public int maxStackSize = 60;
 
     public void Use()
     {
         
     }
-    
 }
