b1841d58632e28ec4787c3843420a2ab0f5b01d5
MarkRobbz <markrobb259@gmail.com>
2024-10-21 12:40:16 +0100

    Scene Save

--- Project/Anteloper/Assets/Scripts/CraftableItem.cs
+++ Project/Anteloper/Assets/Scripts/CraftableItem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CraftableItem : MonoBehaviour
+public class CraftableItem : MonoBehaviour, IUsable
 {
     // Start is called before the first frame update
     void Start()
@@ -15,4 +15,10 @@ public class CraftableItem : MonoBehaviour
     {
         
     }
+
+    public void Use()
+    {
+        
+    }
+    
 }
