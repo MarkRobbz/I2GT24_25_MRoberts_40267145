5e9898706f47d48d1493cf5dd5d8401c6364afc5
Mark Roberts <61202394+MarkRobbz@users.noreply.github.com>
2024-10-29 11:06:13 +0000

    Thirst and hunger declines over time reducing health

--- Project/Anteloper/Assets/Scripts/Player/HungerAndThirst.cs
+++ Project/Anteloper/Assets/Scripts/Player/HungerAndThirst.cs
@@ -1,32 +1,41 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
 
 public class HungerAndThirst : MonoBehaviour
 {
-   [SerializeField] private float _currentHunger;
-   [SerializeField] private float _maxHunger;
-   [SerializeField] private float _currentThirst;
-   [SerializeField] private float _maxThirst;
-   
-   
-    public event Action<float> onHungerChanged; 
+    [Header("Hunger Settings")]
+    [SerializeField] private float _currentHunger = 100f;
+    [SerializeField] private float _maxHunger = 100f;
+    [SerializeField] private float _hungerDecreaseRate = 0.10f; //per second
+
+    [Header("Thirst Settings")]
+    [SerializeField] private float _currentThirst = 100f;
+    [SerializeField] private float _maxThirst = 100f;
+    [SerializeField] private float _thirstDecreaseRate = 0.10f; //per second
+
+    [Header("Health Decrease Due to Hunger or Thirst")]
+    [SerializeField] private float _healthDecreaseRate = 0.50f; //per second
+
+    public event Action<float> onHungerChanged;
     public event Action<float> onThirstChanged;
 
+    private Health _playerHealth;
+
+    // Properties
     public float CurrentHunger
-    { 
+    {
         get { return _currentHunger; }
         private set
         {
-        _currentHunger = Mathf.Clamp(value, 0, _maxHunger);
-        UpdateHungerUI();
+            _currentHunger = Mathf.Clamp(value, 0, _maxHunger);
+            UpdateHungerUI();
         }
     }
-    
+
     public float MaxHunger
-    { 
+    {
         get { return _maxHunger; }
         set
         {
@@ -37,9 +46,9 @@ public class HungerAndThirst : MonoBehaviour
             }
         }
     }
-    
+
     public float CurrentThirst
-    { 
+    {
         get { return _currentThirst; }
         private set
         {
@@ -47,9 +56,9 @@ public class HungerAndThirst : MonoBehaviour
             UpdateThirstUI();
         }
     }
-    
+
     public float MaxThirst
-    { 
+    {
         get { return _maxThirst; }
         set
         {
@@ -61,74 +70,124 @@ public class HungerAndThirst : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        _playerHealth = GetComponent<Health>();
+        if (_playerHealth == null)
+        {
+            Debug.LogError("Health component not found on the player.");
+        }
+        
+        UpdateHungerUI();
+        UpdateThirstUI();
+    }
 
+    private void Update()
+    {
+        DecreaseHungerAndThirst();
+
+        if (CurrentHunger <= 0 || CurrentThirst <= 0)
+        {
+            ReduceHealthDueToHungerOrThirst();
+        }
+    }
+
+    private void DecreaseHungerAndThirst()
+    {
+        float hungerDecreaseAmount = _hungerDecreaseRate * Time.deltaTime;
+        float thirstDecreaseAmount = _thirstDecreaseRate * Time.deltaTime;
+
+        ModifyHunger(-hungerDecreaseAmount);
+        ModifyThirst(-thirstDecreaseAmount);
+    }
+
+    private void ReduceHealthDueToHungerOrThirst()
+    {
+        if (_playerHealth == null) return;
+
+        float minHealth = _playerHealth.MaxHealth * 0.10f; // 10% of max health
+
+        if (_playerHealth.CurrentHealth > minHealth)
+        {
+            float healthDecreaseAmount = _healthDecreaseRate * Time.deltaTime;
+            float potentialNewHealth = _playerHealth.CurrentHealth - healthDecreaseAmount;
+
+            
+            if (potentialNewHealth < minHealth) //Doesn't drop below 10%
+            {
+                healthDecreaseAmount = _playerHealth.CurrentHealth - minHealth;
+            }
+
+            _playerHealth.DecreaseHealth(healthDecreaseAmount);
+        }
+    }
+
+    // Methods to modify hunger and thirst
     public void ModifyHunger(float amount)
     {
-        _currentHunger = Mathf.Clamp(_currentHunger + amount, 0, _maxHunger);
-        UpdateHungerUI();
+        CurrentHunger += amount;
     }
 
     public void ModifyThirst(float amount)
     {
-        _currentThirst = Mathf.Clamp(_currentThirst + amount, 0, _maxThirst);
-        UpdateThirstUI();
+        CurrentThirst += amount;
     }
 
-   public void UpdateHungerUI()
-   {
-       onHungerChanged?.Invoke(_currentHunger);
-   }
+    // UI Update Methods
+    public void UpdateHungerUI()
+    {
+        onHungerChanged?.Invoke(_currentHunger);
+    }
 
-   public void UpdateThirstUI()
-   {
-       onThirstChanged?.Invoke(_currentThirst);
-   }
-   
-   //============ Test methods Inspector =============
+    public void UpdateThirstUI()
+    {
+        onThirstChanged?.Invoke(_currentThirst);
+    }
 
-   [Space(20)]
-   [VerticalGroup("HungerGroup")]
-   [ShowInInspector]
-   private float hungerTestAmount = 10f; 
-   [HorizontalGroup("HungerGroup/Buttons", Width = 150)]
-   [Button("Restore Hunger (Test)"), GUIColor(0.3f, 0.9f, 0.3f)] 
-   private void TestRestoreHunger()
-   {
-       ModifyHunger(hungerTestAmount);
-       Debug.Log("Test-Hunger increased by: " + hungerTestAmount);
-   }
+    
+    
+    
+    //============ Test methods Inspector =============
 
-   [HorizontalGroup("HungerGroup/Buttons", Width = 150)]
-   [Button("Decrease Hunger (Test)"), GUIColor(0.9f, 0.3f, 0.3f)] 
-   private void TestReduceHunger()
-   {
-       ModifyHunger(-hungerTestAmount);
-       Debug.Log("Test-Hunger decreased by: " + hungerTestAmount);
-   }
+    [Space(20)]
+    [VerticalGroup("HungerGroup")]
+    [ShowInInspector]
+    private float hungerTestAmount = 10f;
 
+    [HorizontalGroup("HungerGroup/Buttons", Width = 150)]
+    [Button("Restore Hunger (Test)"), GUIColor(0.3f, 0.9f, 0.3f)]
+    private void TestRestoreHunger()
+    {
+        ModifyHunger(hungerTestAmount);
+        Debug.Log("Test-Hunger increased by: " + hungerTestAmount);
+    }
 
-   [Space(20)]
+    [HorizontalGroup("HungerGroup/Buttons", Width = 150)]
+    [Button("Decrease Hunger (Test)"), GUIColor(0.9f, 0.3f, 0.3f)]
+    private void TestReduceHunger()
+    {
+        ModifyHunger(-hungerTestAmount);
+        Debug.Log("Test-Hunger decreased by: " + hungerTestAmount);
+    }
 
+    [Space(20)]
+    [VerticalGroup("ThirstGroup")]
+    [ShowInInspector]
+    private float thirstTestAmount = 10f;
 
-   [VerticalGroup("ThirstGroup")]
-   [ShowInInspector]
-   private float thirstTestAmount = 10f; 
+    [HorizontalGroup("ThirstGroup/Buttons", Width = 150)]
+    [Button("Restore Thirst (Test)"), GUIColor(0.3f, 0.9f, 0.3f)]
+    private void TestRestoreThirst()
+    {
+        ModifyThirst(thirstTestAmount);
+        Debug.Log("Test-Thirst increased by: " + thirstTestAmount);
+    }
 
-
-   [HorizontalGroup("ThirstGroup/Buttons", Width = 150)]
-   [Button("Restore Thirst (Test)"), GUIColor(0.3f, 0.9f, 0.3f)] 
-   private void TestRestoreThirst()
-   {
-       ModifyThirst(thirstTestAmount);
-       Debug.Log("Test-Thirst increased by: " + thirstTestAmount);
-   }
-
-   [HorizontalGroup("ThirstGroup/Buttons", Width = 150)]
-   [Button("Decrease Thirst (Test)"), GUIColor(0.9f, 0.3f, 0.3f)] 
-   private void TestReduceThirst()
-   {
-       ModifyThirst(-thirstTestAmount);
-       Debug.Log("Test-Thirst decreased by: " + thirstTestAmount);
-   }
-
+    [HorizontalGroup("ThirstGroup/Buttons", Width = 150)]
+    [Button("Decrease Thirst (Test)"), GUIColor(0.9f, 0.3f, 0.3f)]
+    private void TestReduceThirst()
+    {
+        ModifyThirst(-thirstTestAmount);
+        Debug.Log("Test-Thirst decreased by: " + thirstTestAmount);
+    }
 }
