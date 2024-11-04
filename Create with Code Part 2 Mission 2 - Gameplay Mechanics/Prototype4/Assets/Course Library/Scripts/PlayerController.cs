5afb03d36565ed20e593f6184a512c44aa28c11f
MarkRobbz <markrobb259@gmail.com>
2024-11-04 20:10:42 +0000

    Unit 4.3 Complete

--- Create with Code Part 2 Mission 2 - Gameplay Mechanics/Prototype4/Assets/Course Library/Scripts/PlayerController.cs
+++ Create with Code Part 2 Mission 2 - Gameplay Mechanics/Prototype4/Assets/Course Library/Scripts/PlayerController.cs
@@ -8,11 +8,15 @@ public class PlayerController : MonoBehaviour
     public float Speed = 5.0f;
     [SerializeField] private Rigidbody _playerRb;
     [SerializeField] private GameObject _focalPoint;
+    public bool isPickupActive = false;
+    [SerializeField] private float _powerUpStrength = 0.0f;
+    [SerializeField] private GameObject _powerUpIndicator;
 
     private void Start()
     {
         _playerRb = GetComponent<Rigidbody>();
-        
+       
+
     }
 
     private void Update()
@@ -20,5 +24,39 @@ public class PlayerController : MonoBehaviour
         float fowardInput = Input.GetAxis("Vertical");
         
         _playerRb.AddForce(_focalPoint.transform.forward * Speed * fowardInput);
+        _powerUpIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+            if (other.gameObject.CompareTag("Powerup"))
+            {
+                _powerUpIndicator.gameObject.SetActive(true);
+                _powerUpStrength = other.GetComponent<PowerUp>().powerUpStrengh;
+                isPickupActive = true;
+                Destroy(other.gameObject);
+                StartCoroutine(PowerUpContdownRoutine());
+            }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy") && isPickupActive)
+        {
+            Rigidbody enemyRb = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (transform.position - other.gameObject.transform.position).normalized;
+        
+            enemyRb.AddForce(awayFromPlayer * _powerUpStrength, ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator PowerUpContdownRoutine()
+    {
+        
+        yield return new WaitForSeconds(7);
+        isPickupActive = false;
+        _powerUpIndicator.gameObject.SetActive(false);
     }
 }
---
+++ Create with Code Part 2 Mission 2 - Gameplay Mechanics/Prototype4/Assets/Course Library/Scripts/PowerUp.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public float powerUpStrengh = 20.0f;
+
+   
+}
